f9e5540c5c932ae5f7075d74a3527af4a1e6c77e
Gil <gilbertjvirgo@gmail.com>
2020-10-31 13:45:42 +0000

    Syntax changes to index styles

--- site/styles/index.js
+++ site/styles/index.js
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
-import theme from '../theme';
-
+import styled from "styled-components";
+import theme from "../theme";
 
 export const Grid = styled.div`
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    max-width: 1125px;
-    margin: 100px auto;
-    grid-column-gap: 15px;
-    grid-row-gap: 45px;
-    ${theme.breakpoint('md')`
+	display: grid;
+	grid-template-columns: repeat(6, 1fr);
+	max-width: 1125px;
+	margin: 100px auto;
+	grid-column-gap: 15px;
+	grid-row-gap: 45px;
+
+	${theme.breakpoint("md")`
         grid-template-columns: repeat(12, 1fr);
     `}
 `;
 
 export const BodyText = styled.p`
-    font-family: 'myriad-pro';
+	font-family: "myriad-pro";
 `;
