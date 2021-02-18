b14234e40dc17cbfdbcbf929d061910cc61cbc41
TkreateDev <tanbir98@hotmail.it>
2021-02-18 17:23:36 +0000

    added footer, added centering box

--- site/components/Footer/Footer.js
+++ site/components/Footer/Footer.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import { Wrapper } from './styles';
+import { BodyText, CenteredBodyText, Grid, SmallText } from '../../styles';
+import { GridFooter, LeftSideFooter, MediumBoxText, RightSideFooter, Wrapper } from './styles';
 
-export const Footer = ({ children, ...props }) => {
-	return <Wrapper {...props}>{children}</Wrapper>
+export const Footer = ({...props }) => {
+	return (
+		<div style={{backgroundColor: 'black'}}>
+			<GridFooter>
+					<LeftSideFooter>
+						<MediumBoxText>
+						Remember your leaders, those who spoke to you the word of God. Consider the outcome of their way of life, and imitate their faith.
+						</MediumBoxText>
+						<SmallText>
+						HEBREWS 13:7 ESV
+						</SmallText>
+					</LeftSideFooter>
+					<RightSideFooter>
+						hello hello hello
+					</RightSideFooter>
+			</GridFooter>
+		</div>
+		)
 }
